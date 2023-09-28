438d2308892767020873eeed9c4ef192d6b2d772
Númi Sveinsson <numisveins@Numis-MBP.attlocal.net>
2023-09-28 15:56:40 -0700

    metric calc works

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,13 +1,17 @@
 import os
+import SimpleITK as sitk
+import numpy as np
 
 def dice(pred, truth):
 
     if not isinstance(pred, np.ndarray):
         pred = sitk.GetArrayFromImage(pred)
         truth = sitk.GetArrayFromImage(truth)
-    
-    pred = pred.astype(np.int)
-    true = truth.astype(np.int)
+    pred = pred.astype(np.int32)
+    true = truth.astype(np.int32)
+    if true.max() > 1:
+        true = true // true.max()
+
     num_class = np.unique(true)
 
     #change to one hot
@@ -19,7 +23,8 @@ def dice(pred, truth):
 
     mask =( pred > 0 )+ (true > 0)
     dice_out[0] = np.sum((pred==true)[mask]) * 2. / (np.sum(pred>0) + np.sum(true>0))
-    return dice_out
+    
+    return dice_out[0]
 
 def masked_dice(self, masked_dir):
         mask = sitk.ReadImage(masked_dir)
@@ -32,6 +37,12 @@ def masked_dice(self, masked_dir):
         return dice
 
 def hausdorff(pred, truth):
+
+    # check pixel type
+    if truth.GetPixelID() != sitk.sitkUInt8:
+        truth = sitk.Cast(truth, sitk.sitkUInt8)
+    if pred.GetPixelID() != sitk.sitkUInt8:
+        pred = sitk.Cast(pred, sitk.sitkUInt8)
 
     haus_filter = sitk.HausdorffDistanceImageFilter()
     haus_filter.Execute(pred, truth)
@@ -48,15 +59,22 @@ def only_keep_mask(pred, mask):
 
 def process_case_name(case_name):
     'Change this function if naming convention changes'
-    return case_name[4:12]
+    return case_name[15:24]
 
-def read_in(case, pred_folder, truth_folder):
-    import SimpleITK as sitk
+def read_truth(case, truth_folder):
 
-    pred = sitk.ReadImage(pred_folder+case+'.vtk')
-    truth = sitk.ReadImage(truth_folder+case+'.vtk')
-    
-    return pred, truth
+    try:
+        truth = sitk.ReadImage(truth_folder+case+'.vtk')
+    except:
+        truth = sitk.ReadImage(truth_folder+case+'.mha')
+
+    return truth
+
+def read_seg(pred_folder, seg):
+
+    pred = sitk.ReadImage(pred_folder+seg)
+
+    return pred
 
 def pre_process(pred):
     
@@ -66,20 +84,20 @@ def pre_process(pred):
 
 def calc_metric(metric, pred, truth, mask):
 
-    if metric = 'dice':
+    if metric == 'dice':
 
         score = dice(pred, truth)
 
-    elif metric = 'hausdorff':
+    elif metric == 'hausdorff':
 
         score = hausdorff(pred, truth)
 
-    elif metric = 'dice mask':
+    elif metric == 'dice mask':
 
         masked_pred = only_keep_mask(pred, mask)
         score = dice(masked_pred, truth)
 
-    elif metric = 'hausdorff mask':
+    elif metric == 'hausdorff mask':
 
         masked_pred = only_keep_mask(pred, mask)
         score = hausdorff(masked_pred, truth)
@@ -91,31 +109,53 @@ if __name__=='__main__':
     preprocess_pred = True
 
     #input folder of segmentation results
-    pred_folder = ''
-    truth_folder = ''
+    pred_folder = '/Users/numisveins/Downloads/preds/'
+    truth_folder = '/Users/numisveins/Downloads/truths/'
     mask_folder = ''
 
     # output folder for plots
-    output_folder = ''
+    output_folder = '/Users/numisveins/Downloads/'
 
     #metrics
-    metrics = ['dice', 'hausdorff']#, 'dice mask', 'hausdorff mask']
+    metrics = ['hausdorff', 'dice']#, 'dice mask', 'hausdorff mask']
 
     for metric in metrics:
 
+        print(f"\nCalculating {metric}...")
         scores = []
 
         for seg in os.listdir(pred_folder):
             
             case = process_case_name(seg)
 
-            pred, truth = read_in(case, pred_folder, truth_folder)
+            pred = read_seg(pred_folder, seg)
+            truth = read_truth(case, truth_folder)
 
             if preprocess_pred:
                 pred = pre_process(pred)
 
             if 'mask' in metric:
-                mask = read_in(case, mask_folder, truth_folder)
+                mask = read_truth(case, mask_folder)
             else: mask = None
 
             score = calc_metric(metric, pred, truth, mask)
+
+            scores.append(score)
+
+            print(f"{case}: {score}")
+
+        print(f"Average {metric}: {np.mean(scores)}")
+
+        # Make box plot
+        import matplotlib.pyplot as plt
+        plt.figure()
+        plt.boxplot(scores)
+        plt.title(f'{metric} scores')
+        # set y axis lower limit to 0
+        plt.ylim(bottom=0)
+        plt.ylabel(f'{metric} score')
+        if metric.includes('dice'):
+            plt.ylim(top=1)
+        plt.savefig(output_folder + f'{metric}_scores.png')
+        plt.close()
+
