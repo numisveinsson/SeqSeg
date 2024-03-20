6f96f6f20f2277c7ac65a444a5a271a416003bc7
numisveinsson <numisveinsson@gmail.com>
2024-03-20 14:19:30 -0700

    get old ref from 2 steps back

--- modules/assembly.py
+++ modules/assembly.py
@@ -683,8 +683,13 @@ def get_old_ref_point(vessel_tree, step_seg, i, mega_sub = False, mega_sub_N = 0
     if not mega_sub:
         # return the old point
         if i != 0:
-            prev_step = vessel_tree.get_previous_step(i)
-            old_point_ref = prev_step['old point']
+            # return the old of two steps before
+            branch = len(vessel_tree.branches) - 1
+            prev_n = vessel_tree.get_previous_n(branch, 2)
+            step = vessel_tree.steps[prev_n[0]]
+            old_point_ref = step['old point']
+            # prev_step = vessel_tree.get_previous_step(i)
+            # old_point_ref = prev_step['old point']
         elif i == 0:
             old_point_ref = step_seg['old point']
     else:
