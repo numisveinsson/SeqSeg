385a974ae64d48384edc5a45a73611d2aa0506e2
numisveinsson <numisveinsson@gmail.com>
2024-05-31 17:27:35 -0700

    new test on already segmented file

---
+++ tests/test_segmentation.py
@@ -0,0 +1,18 @@
+import sys
+import os
+if __name__ == '__main__':
+    """
+    Test SeqSeg on a case of already segmented data
+
+    This can be run without the need for:
+    - pytorch
+    - vmtk
+
+    Only requires:
+    - numpy
+    - vtk
+    - sitk
+    """
+
+    # Load the data from subfolder 'test_data'
+    data_path = os.path.join(os.path.dirname(__file__), 'test_data')
