acb0249b033cf452dc99041a829a9971e0c75132
numisveinsson <numisveinsson@gmail.com>
2024-06-02 13:24:58 -0700

    new test

---
+++ tests/test.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# Greetings
+echo "Running tests..."
+
+# Activate virtual environment
+conda activate ml_data
+
+# CD to the directory of the script /Users/numisveins/Documents/SeqSeg
+cd /Users/numisveins/Documents/SeqSeg
+
+# Run the tests
+python3 auto_centerline.py \
+    -data_dir /Users/numisveins/Documents/SeqSeg/tests/test_data/ \
+    -config_name global_test \
+    -img_ext .nii.gz \
+    -outdir /Users/numisveins/Documents/SeqSeg/tests/test_output/ \
+    -start 0 \
+    -stop 1 \
+    -max_n_steps 1500 \
