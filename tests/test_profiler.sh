8b43b0ca6ee18cf105b932ff4ed6f0a10a3cc8ed
numisveinsson <numisveinsson@gmail.com>
2024-08-14 15:46:05 -0700

    test scripts

--- tests/test_profiler.sh
+++ tests/test_profiler.sh
@@ -10,11 +10,11 @@ conda activate ml_data
 cd /Users/numisveins/Documents/SeqSeg
 
 # Run the tests
-python3 auto_centerline.py \
+python3 -m cProfile -o test_pstats auto_centerline.py \
     -data_dir /Users/numisveins/Documents/SeqSeg/tests/test_data/ \
     -config_name global_test \
     -img_ext .nii.gz \
     -outdir /Users/numisveins/Documents/SeqSeg/tests/test_output/ \
     -start 0 \
     -stop 1 \
-    -max_n_steps 50 \
+    -max_n_steps 500 \
