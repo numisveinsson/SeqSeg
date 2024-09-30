414b6842baeaa3afb1c3868d050da885794ab904
Numi SC <numisveinsson@gmail.com>
2024-09-30 14:23:26 -0700

    print if GPU avail

--- modules/tracing.py
+++ modules/tracing.py
@@ -129,6 +129,7 @@ def trace_centerline(
         if gpu_avail:
             # check if GPU is available
             if torch.cuda.is_available():
+                print('GPU available, using GPU')
                 device_use = torch.device('cuda', 0)
             else:
                 print('GPU not available, using CPU')
