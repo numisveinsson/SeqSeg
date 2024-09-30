371ec439662be9c243b5007e7dc85f194b3cfde9
Numi SC <numisveinsson@gmail.com>
2024-09-30 14:23:37 -0700

    new job file for cardiac trace

---
+++ job_cardiac_trace_gpu.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=auto_cent
+#
+# Account:
+#SBATCH --account=fc_biome
+#
+# Partition:
+#SBATCH --partition=savio3_gpu
+#
+# QoS:
+#SBATCH --qos=gtx2080_gpu3_normal
+#
+# Number of nodes:
+#SBATCH --nodes=1
+#
+# Number of tasks needed for use case (example):
+#SBATCH --ntasks=1
+#
+# Processors per task:
+#SBATCH --cpus-per-task=2
+#
+# Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
+#SBATCH --gres=gpu:GTX2080TI:1
+#
+# Wall clock limit:
+#SBATCH --time=12:00:00
+#
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=numi@berkeley.edu
+#
+## Command(s) to run (example):
+echo "GTX gpu"
+source activate /global/scratch/users/numi/environments/seqseg2
+
+export nnUNet_raw="/global/scratch/users/numi/nnUnet_data/nnUnet_raw"
+export nnUNet_preprocessed="/global/scratch/users/numi/nnUnet_data/nnUNet_preprocessed"
+export nnUNet_results="/global/scratch/users/numi/nnUnet_data/nnUNet_results"
+
+cd /global/scratch/users/numi/SeqSeg/
+
+python3 auto_centerline.py \
+    -test_name 3d_fullres \
+    -train_dataset Dataset005_SEQAORTANDFEMOMR \
+    -fold all \
+    -img_ext .nii.gz \
+    -outdir output_data_combo/ \
+    -scale 0.1 \
+    -start 5 \
+    -stop 6 \
+    -max_n_steps 500 \
+    -max_n_branches 50 \
+    -unit mm \
+    -config_name global_aorta \
+    -data_dir /global/scratch/users/numi/data_combo_paper_test/ \
+    -gpu True \
+
+# Dataset010_SEQCOROASOCACT Dataset006_SEQAORTANDFEMOCT Dataset005_SEQAORTANDFEMOMR Dataset016_SEQPULMPARSECT
+# Dataset018_SEQAORTASONEMR Dataset017_SEQAORTASONECT
+# module load gcc
+# module load cuda/10.0
+# module load cudnn/7.5
