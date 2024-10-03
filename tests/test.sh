11e17f5cc1f1b13f919889d6c604aef52ce259b9
numisveinsson <numisveinsson@gmail.com>
2024-10-03 11:19:21 -0700

    changes

--- tests/test.sh
+++ tests/test.sh
@@ -11,10 +11,10 @@ cd /Users/numisveins/Documents/SeqSeg
 
 # Run the tests
 python3 auto_centerline.py \
-    -data_dir /Users/numisveins/Documents/SeqSeg/tests/test_data/ \
+    -data_dir tests/test_data/ \
     -config_name global_test \
     -img_ext .nii.gz \
-    -outdir /Users/numisveins/Documents/SeqSeg/tests/test_output/ \
+    -outdir tests/test_output/ \
     -start 0 \
     -stop 1 \
     -max_n_steps 50 \
